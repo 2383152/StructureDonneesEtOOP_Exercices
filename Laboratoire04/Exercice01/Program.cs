bc7f523bb1ff0eb4fe8bcbe3cfcfa08de27a91f4
2383152 <2383152@L121-24-13188>
2026-02-18 11:00:56 -0500

    rr

--- Laboratoire04/Exercice01/Program.cs
+++ Laboratoire04/Exercice01/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Solide cube = new Solide(20, 30, 50, "cube", "carrée");
+            cube.AffichageInfoSolide();
+
+            ObjetPhysique solide1 = cube;
+
+            Liquide eau = new Liquide(10, 40, 60, "eau", "bleu");
+            eau.AffichageInfoLiquide();
+
+            ObjetPhysique liquide1 = eau;
         }
     }
 }
