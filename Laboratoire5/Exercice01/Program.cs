51120efd8225e190dacd33cc4fc5ff50598adb5d
2383152 <2383152@L121-24-13188>
2026-02-23 16:58:05 -0500

    yy

--- Laboratoire5/Exercice01/Program.cs
+++ Laboratoire5/Exercice01/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            EtatMatiere matiere1 = new Solide();
+            matiere1.AfficherEtat();
+
+            //simulation pour changer l'état
+            matiere1 = matiere1.ChangerEtat(-10); //reste un solide
+            matiere1.AfficherEtat();
+
+            matiere1 = matiere1.ChangerEtat(10);//Devient un liquide
+            matiere1.AfficherEtat();
+
+            matiere1 = matiere1.ChangerEtat(150);//Devient un gaz
+            matiere1.AfficherEtat();
+
+            matiere1 = matiere1.ChangerEtat(50);//Redevient un liquide
+            matiere1.AfficherEtat();
+
+            matiere1 = matiere1.ChangerEtat(-5);//Redevient un solide
+            matiere1.AfficherEtat();
         }
     }
 }
