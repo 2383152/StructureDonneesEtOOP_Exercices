3c1706ef062f45684b2da7cab8487022c38f342f
gmari <gmari@MARILOU_PC>
2026-03-01 14:08:01 -0500

    uu

--- Laboratoire02/Exercice01/Complex.cs
+++ Laboratoire02/Exercice01/Complex.cs
@@ -8,5 +8,47 @@ namespace Exercice01
 {
     internal class Complex
     {
+
+        //Ma facons de faire get set n'est pas bonne, mais jsp pk
+
+        /*
+        public int nombreReel { get; set; }
+        public int nombreImaginaire { get; set; }
+        */
+
+        //Voici la bonne facons pour cette exercice:
+        double nbReel, nbImaginaire;
+
+        public double GetA() { return nbReel; }
+        public double GetB() { return nbImaginaire; }
+
+        public void SetA(double value) { nbReel = value; }
+        public void SetB(double value) { nbImaginaire = value; }
+
+
+
+
+
+        //Constructeur
+        public Complex(int nombreReel, int nombreImaginaire)
+        {
+            this.nbReel = nombreReel;
+            this.nbImaginaire = nombreImaginaire;
+        }
+
+        public string AfficherValeur()
+        {
+            return $"({nbReel}, {nbImaginaire})";            
+        }
+
+        public double GetMagnitude() 
+        { 
+            return Math.Sqrt((nbReel * nbReel) + (nbImaginaire * nbImaginaire)); 
+        }
+
+        public double Sum()
+        {
+            return 
+        }
     }
 }
