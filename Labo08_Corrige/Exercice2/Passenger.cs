ff8245057c6faf71779397a107f2ab819e09c2c5
2383152 <2383152@L121-24-13188>
2026-03-30 16:47:33 -0400

    ttt

---
+++ Labo08_Corrige/Exercice2/Passenger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice2
+{
+    internal class Passenger
+    {
+        string nom;
+        int age;
+
+        //Définition des méthodes
+        public void Ambarquer()
+        {
+            Console.WriteLine($"{nom} embarque a bord");
+        }
+
+        public void Desembarquer()
+        {
+            Console.WriteLine($"{nom} débarque");
+        }
+    }
+}
